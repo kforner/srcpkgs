1204eee09e5c6087f8881887f6f84e5bcf4c0a03
Karl Forner <karl.forner@gmail.com>
2024-04-23 14:58:21 +0200

    removed $MD5 from srcpkg. removed /test_library_srcpkgs/minimal test package. fixed one test in test_that("as_srcpkg).

--- log/dev.R
+++ log/dev.R
@@ -12,6 +12,7 @@ test(pkg, 'meta')
 test(pkg, 'pkg_has_changed')
 test(pkg, 'pkg_load')
 test(pkg, 'pkg_utils')
+test(pkg, 'plan')
 test(pkg, 'root')
 test(pkg, 'roxy')
 test(pkg, 'srcpkg$')
@@ -22,10 +23,7 @@ test(pkg, 'utils')
 load_all(pkg)
 
 
-minimal <- devtools::as.package('tests/testthat/test_library_srcpkgs/minimal')
 
-srcpkg <- srcpkgs:::srcpkg(minimal)
-print(srcpkg)
 
 covr::report(covr::package_coverage())
 
